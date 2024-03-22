6168cd62166c4b3d3e9513c3cc203f059c2803e0
Ysrae1 <Ysrae1@outlook.com>
2024-03-22 23:11:19 +0000

    Added function

---
+++ Exercise1_2(functional).py
@@ -0,0 +1,144 @@
+#Multiprocessing Version
+import sys
+import numpy as np
+import torch
+from Exercise1_1 import LQRSolver
+from torch.multiprocessing import Pool, set_start_method
+import time
+def MonteCarloSampler(iteration, params):
+    
+    C = params['C']
+    D = params['D']
+    N = params['N']
+    R = params['R']
+    S = params['S']
+    X0 = params['X0']
+    H = params['H']
+    dt = params['dt']
+    multX = params['multX']
+    multa = params['multa']
+    sig = params['sig']
+         
+    X_0_N = X0
+    
+    for i in range(N-1):
+        
+        X_next = ((torch.eye(2)+ dt[i]*(H + multX@S[i]))@ X_0_N[i:].transpose(1,2)+sig*torch.sqrt(dt[i])*torch.randn(1)).transpose(1,2)
+        X_0_N = torch.cat((X_0_N, X_next), dim=0)
+
+    alp = multa@X_0_N.transpose(1,2)
+    int_ = X_0_N@C@X_0_N.transpose(1,2) + alp.transpose(1,2)@D@alp
+    J = X_0_N[-1]@R@X_0_N[-1].T + torch.tensor(0.5)*dt@((int_.squeeze(1)[1:]+int_.squeeze(1)[:-1]))
+
+    return J
+
+def print_progress(outer, total_outer, inner, total_inner):
+    sys.stdout.write('\x1b[2J\x1b[H') 
+    if outer >= 0: 
+        print("Total progress:")
+        percent_outer = outer / total_outer  
+        bar_length_outer = int(30 * percent_outer)
+        bar_outer = '[' + '=' * bar_length_outer + ' ' * (30 - bar_length_outer) + ']'
+        print(f"{bar_outer} {percent_outer * 100:.2f}% ({outer}/{total_outer})")
+    
+    if inner >= 0:  
+        print("Inner progress:")
+        percent_inner = (inner + 1) / total_inner
+        bar_length_inner = int(30 * percent_inner)
+        bar_inner = '[' + '=' * bar_length_inner + ' ' * (30 - bar_length_inner) + ']'
+        print(f"{bar_inner} {percent_inner * 100:.2f}% ({inner+1}/{total_inner})")
+
+
+if __name__ == '__main__':
+    
+    try:
+        set_start_method('spawn')  
+    except RuntimeError:
+        pass
+    
+    N = int(sys.argv[2])
+    batch_size_MC = int(sys.argv[3])
+    
+    load_interval_setting = torch.load(sys.argv[1]+'/interval_setting.pt')
+
+    t_ends = load_interval_setting['t_ends']
+    t_num = load_interval_setting['t_num']
+    x_ends = load_interval_setting['x_ends']
+    x_num = load_interval_setting['x_num']
+
+    t_batch_i = torch.linspace(t_ends[0],t_ends[1],t_num,dtype=torch.double)
+
+    x1 = torch.linspace(x_ends[0][0],x_ends[0][1],x_num[0],dtype=torch.double)
+    x2 = torch.linspace(x_ends[1][0],x_ends[1][1],x_num[1],dtype=torch.double)
+
+    x_batch_i = torch.cartesian_prod(x1, x2).unsqueeze(1)
+
+    file_path_Ex1_2 = f'Exercise1_2'
+    init_for_solver = torch.load(file_path_Ex1_2+'/'+'initialization_for_solver.pt')
+    
+    H = init_for_solver['H']
+    M = init_for_solver['M']
+    sigma = init_for_solver['sigma'] 
+    C = init_for_solver['C'] 
+    D = init_for_solver['D'] 
+    R = init_for_solver['R']
+    T = init_for_solver['T']
+    method = init_for_solver['method']
+    
+    solver = LQRSolver(H, M, sigma, C, D, R, T, method)
+
+    initial_tensor_file = torch.tensor([])
+    torch.save(initial_tensor_file, sys.argv[1]+'value_MC.pt')
+
+    for outer in range(len(t_batch_i)):
+
+        t0 = t_batch_i[outer]
+        time_grid_for_MC = torch.linspace(t0,T,N,dtype = torch.double)
+        dt_for_MC = time_grid_for_MC[1:]-time_grid_for_MC[:-1]
+        S = solver.solve_riccati_ode(time_grid_for_MC.unsqueeze(0)).squeeze()
+        multp_X = - M@torch.linalg.inv(D)@M.T
+        multp_alp = - torch.linalg.inv(D)@M.T@S
+        params = {
+        'C': C,
+        'D': D,
+        'N': N,
+        'R': R,
+        'S': S,
+        'X0': 0,
+        'H': H,
+        'dt': dt_for_MC,
+        'multX': multp_X,
+        'multa':multp_alp,
+        'S': S,
+        'sig': sigma,
+        }
+
+        for inner in range(len(x_batch_i)):
+
+            print_progress(outer, len(t_batch_i), inner, len(x_batch_i))  
+
+            X0 = x_batch_i[inner].unsqueeze(0)
+            params['X0'] = X0
+    
+            J_list = []
+
+            times_MC = 10
+
+            for i in range(times_MC):
+
+                iterations = list(range(batch_size_MC))  
+                
+                with Pool(processes = 8) as pool:  
+
+                    J_sample = pool.starmap(MonteCarloSampler, [(iteration, params) for iteration in iterations])
+
+                J_sample = torch.stack(J_sample)
+
+                J_list.append(torch.mean(J_sample).unsqueeze(0))
+
+            J_list_tensor = torch.cat(J_list, dim=0)
+            J_list_unadded = torch.load(initial_tensor_file, sys.argv[1]+'value_MC.pt')
+            J_list_add = torch.cat((J_list_unadded, J_list_tensor.unsqueeze(0)), dim=0)
+        print_progress(outer + 1, len(t_batch_i), -1, len(x_batch_i))  
+
+    print("\nCalculation Finished.")
--- Exercise1_2.py
+++ Exercise1_2.py
@@ -18,28 +18,19 @@ def MonteCarloSampler(iteration, params):
     multX = params['multX']
     multa = params['multa']
     sig = params['sig']
+         
+    X_0_N = X0
     
-    J_sample = []
-    
-    for ii in range(10):
+    for i in range(N-1):
         
-        X_0_N = X0
-        
-        for i in range(N-1):
-            
-            X_next = ((torch.eye(2)+ dt[i]*(H + multX@S[i]))@ X_0_N[i:].transpose(1,2)+sig*torch.sqrt(dt[i])*torch.randn(1)).transpose(1,2)
-            X_0_N = torch.cat((X_0_N, X_next), dim=0)
+        X_next = ((torch.eye(2)+ dt[i]*(H + multX@S[i]))@ X_0_N[i:].transpose(1,2)+sig*torch.sqrt(dt[i])*torch.randn(1)).transpose(1,2)
+        X_0_N = torch.cat((X_0_N, X_next), dim=0)
 
-        alp = multa@X_0_N.transpose(1,2)
-        int_ = alp.transpose(1,2)@D@alp + X_0_N@C@X_0_N.transpose(1,2)
-        J = X_0_N[-1]@R@X_0_N[-1].T + torch.tensor(0.5)*dt@((int_.squeeze(1)[1:]+int_.squeeze(1)[:-1]))
+    alp = multa@X_0_N.transpose(1,2)
+    int_ = X_0_N@C@X_0_N.transpose(1,2) + alp.transpose(1,2)@D@alp
+    J = X_0_N[-1]@R@X_0_N[-1].T + torch.tensor(0.5)*dt@((int_.squeeze(1)[1:]+int_.squeeze(1)[:-1]))
 
-        J_sample.append(J)
-        
-    J_sample = torch.stack(J_sample)
-
-
-    return torch.mean(J_sample).unsqueeze(0)
+    return J
 
 if __name__ == '__main__':
     
@@ -48,18 +39,19 @@ if __name__ == '__main__':
     except RuntimeError:
         pass
     
-    H = torch.tensor([[1.0, 2.0], [-2.0, -3.0]], dtype=torch.double)
-    M = torch.tensor([[1.0,0.0], [0.0,1.0]], dtype=torch.double)
-    sigma = torch.tensor([[[0.5249],[0.4072]]], dtype=torch.double) 
-    C = torch.tensor([[2.0, 0.0], [0.0, 1.0]], dtype=torch.double)  # Positive semi-definite
-    D = torch.tensor([[1.0, 0.0], [0.0, 1.0]], dtype=torch.double)  # Positive definite
-    R = torch.tensor([[1.0, 0.0], [0.0, 1.0]], dtype=torch.double)  # Positive semi-definite
+    H = torch.tensor([[1.2, 0.8], [-0.6, 0.9]], dtype=torch.double)
+    M = torch.tensor([[0.5,0.7], [0.3,1.0]], dtype=torch.double)
+    sigma = torch.tensor([[[0.8],[1.1]]], dtype=torch.double) 
+    C = torch.tensor([[1.6, 0.0], [0.0, 1.1]], dtype=torch.double)  # Positive semi-definite
+    D = torch.tensor([[0.5, 0.0], [0.0, 0.7]], dtype=torch.double)  # Positive definite
+    R = torch.tensor([[0.9, 0.0], [0.0, 1.0]], dtype=torch.double)  # Positive semi-definite
     T = torch.tensor(1.0, dtype=torch.double)
     method = 'rk4'
     
     solver = LQRSolver(H, M, sigma, C, D, R, T, method)
     
     N = 5000
+
     t0 = torch.tensor(0.1,dtype = torch.double)
     
     time_grid_for_MC = torch.linspace(t0,T,N,dtype = torch.double)
@@ -67,8 +59,8 @@ if __name__ == '__main__':
     X0 = 0.5*torch.ones([1,1,2], dtype=torch.double)
     dt_for_MC = time_grid_for_MC[1:]-time_grid_for_MC[:-1]
     S = solver.solve_riccati_ode(time_grid_for_MC.unsqueeze(0)).squeeze()
-    multpX = - M@torch.linalg.inv(D)@M.T
-    multpalp = -torch.linalg.inv(D)@M.T@S
+    multp_X = - M@torch.linalg.inv(D)@M.T
+    multp_alp = - torch.linalg.inv(D)@M.T@S
     
     params = {
     'C': C,
@@ -79,16 +71,32 @@ if __name__ == '__main__':
     'X0': X0,
     'H': H,
     'dt': dt_for_MC,
-    'multX': multpX,
-    'multa':multpalp,
+    'multX': multp_X,
+    'multa':multp_alp,
     'S': S,
     'sig': sigma,
     }
     
-    iterations = list(range(500))  
-    
-    with Pool(processes = 12) as pool:  
-        J_list = pool.starmap(MonteCarloSampler, [(iteration, params) for iteration in iterations])
+    J_list = []
+
+    times_MC = 20
+    batch_size_MC = 5000
+
+    for i in range(times_MC):
+
+        iterations = list(range(batch_size_MC))  
         
+        with Pool(processes = 10) as pool:  
+
+            J_sample = pool.starmap(MonteCarloSampler, [(iteration, params) for iteration in iterations])
+
+        J_sample = torch.stack(J_sample)
+
+        J_list.append(torch.mean(J_sample).unsqueeze(0))
+
     J_list_tensor = torch.cat(J_list, dim=0)  
-    print(torch.sort(J_list_tensor)[0][:20])
+
+    print(f"\nThe optimal value from the numerical solution is: \n{solver.value_function(t0.unsqueeze(0),X0).item()} \n ")
+
+    print(f"The optimal values from {times_MC} time(s) of Monte Carlo Simulation (batch size {batch_size_MC} for each run) is：\n \n{torch.sort(J_list_tensor)[0]} \n")
+
