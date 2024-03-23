3d0591b4809bbd0c7f5d3972f5812de576a692ca
Ysrae1 <Ysrae1@outlook.com>
2024-03-23 07:02:03 +0000

    builded requirements.txt

--- run_MCs.sh
+++ run_MCs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python Exercise1_2\(functional\).py Exercise1_2/value_MC/5x5/FSS_1e5/1_step 1 100000 &
-python Exercise1_2\(functional\).py Exercise1_2/value_MC/5x5/FSS_1e5/10_steps 10 100000 &
-python Exercise1_2\(functional\).py Exercise1_2/value_MC/5x5/FSS_1e5/50_steps 50 100000 &
-python Exercise1_2\(functional\).py Exercise1_2/value_MC/5x5/FTSN_5e3/10_samples 5000 10 &
-python Exercise1_2\(functional\).py Exercise1_2/value_MC/5x5/FTSN_5e3/50_samples 5000 50 &
-python Exercise1_2\(functional\).py Exercise1_2/value_MC/5x5/FTSN_5e3/100_samples 5000 100 &
+python Exercise1_2_functional.py Exercise1_2/value_MC/5x5/FSS_1e5/1_step 1 100000 &
+python Exercise1_2_functional.py Exercise1_2/value_MC/5x5/FSS_1e5/10_steps 10 100000 &
+python Exercise1_2_functional.py Exercise1_2/value_MC/5x5/FSS_1e5/50_steps 50 100000 &
+python Exercise1_2_functional.py Exercise1_2/value_MC/5x5/FTSN_5e3/10_samples 5000 10 &
+python Exercise1_2_functional.py Exercise1_2/value_MC/5x5/FTSN_5e3/50_samples 5000 50 &
+python Exercise1_2_functional.py Exercise1_2/value_MC/5x5/FTSN_5e3/100_samples 5000 100 &
